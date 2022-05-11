e40044696563dc0993f6084c380454cc01da932a
Krzysztof Skubała <Krzysztof.Skubala@cshark.com>
2022-05-11 11:57:44 +0200

    PeernetWindow control

---
+++ Peernet.SDK/WPF/PeernetWindow.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Peernet.SDK.WPF
+{
+    public class PeernetWindow : Window
+    {
+        public static new readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register(
+                "Title",
+                typeof(string),
+                typeof(PeernetWindow));
+
+        public new string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        protected void Close_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        public override void OnApplyTemplate()
+        {
+            Button closeButton = GetTemplateChild("closeButton") as Button;
+            if (closeButton != null)
+            {
+                closeButton.Click += Close_OnClick;
+            }
+
+            base.OnApplyTemplate();
+        }
+    }
+}
